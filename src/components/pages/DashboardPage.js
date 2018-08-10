64347224d376dbfa0c0c716278613423685ba801
Harichandra Harmalkar <harichandra.harmalkar@synerzip.com>
2018-08-10 18:14:23 +0530

    logout option on dashboard page

--- src/components/pages/DashboardPage.js
+++ src/components/pages/DashboardPage.js
@@ -1,27 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-
+import { userLoggingOut } from "../../actions/auth";
 
 class DashboardPage extends React.Component {
   render() {
-    const { isConfirmed } = this.props;
+    const { isConfirmed, isAuthenticated, logout } = this.props;
     return (
       <div className="container-fluid">
         <h1>Dashboard Page</h1>
+        {isAuthenticated && <button onClick={logout}>Logout</button>  }
       </div>
     );
   }
 }
 
 DashboardPage.propTypes = {
-  isConfirmed: PropTypes.bool.isRequired
+  isConfirmed: PropTypes.bool.isRequired,
+  isAuthenticated:PropTypes.bool.isRequired
 };
 
 function mapStateToProps(state) {
   return {
-    isConfirmed: !!state.user.confirmed
+    isConfirmed: !!state.user.confirmed,
+    isAuthenticated: !!state.user.token
+    
   };
 }
 
-export default connect(mapStateToProps)(DashboardPage);
+export default connect(mapStateToProps, {logout:userLoggingOut})(DashboardPage);
