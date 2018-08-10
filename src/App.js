0af95030f11d183d7480e0a7d3972c8ede8525d1
Harichandra Harmalkar <harichandra.harmalkar@synerzip.com>
2018-08-10 18:08:44 +0530

    routing based on token
    changes made :
    * guest routes for unauthenticated users  and user routes for
    authenticated users

--- src/App.js
+++ src/App.js
@@ -3,17 +3,30 @@ import {Route} from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
 import SignUpPage from './components/pages/SignUpPage';
+import DashboardPage from './components/pages/DashboardPage';
 import ForgotPasswordPage from './components/pages/ForgotPasswordPage';
+import GuestRoute from './routes/GuestRoute';
+import UserRoute from './routes/UserRoute';
+import PropTypes from "prop-types";
 
+class App extends React.Component {
+	
+render(){
+	const { location } = this.props;
+return (<div class="container">
+		<GuestRoute path="/" location={location} exact component={HomePage} />
+		<GuestRoute path="/login" location={location} exact component={LoginPage} />
+		<GuestRoute path="/signup" location={location} exact component={SignUpPage} />
+		<GuestRoute path="/forgotpassword" location={location} exact component={ForgotPasswordPage} />
+		<UserRoute path="/dashboard" location={location} exact component={DashboardPage} />
+	</div>);
+}
+}
 
-
-const App = () => (
-	<div class="container-fluid">
-		<Route path="/" exact component={HomePage} />
-		<Route path="/login" exact component={LoginPage} />
-		<Route path="/signup" exact component={SignUpPage} />
-		<Route path="/forgotpassword" exact component={ForgotPasswordPage} />
-	</div>
-)
-
+App.propTypes = {
+	location: PropTypes.shape({
+	  pathname: PropTypes.string.isRequired
+	}).isRequired
+  };
+  
 export default App;
